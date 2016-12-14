ddfd86b9438b1b1bee1b2f64884a69b04e624d9a
RAZAN-PC <RAZAN-PC>
2016-12-14 17:52:46 +0545

    include home part

--- src/main/java/com/model/spring/AboutUsModel.java
+++ src/main/java/com/model/spring/AboutUsModel.java
@@ -1,5 +1,6 @@
 package com.model.spring;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,32 +15,42 @@ public class AboutUsModel {
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private String title;
+	
+	@Column(name = "details")
 	private String details;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getDetails() {
 		return details;
 	}
+
 	public void setDetails(String details) {
 		this.details = details;
 	}
-	
-	public AboutUsModel(){}
-	public AboutUsModel(String title,String details)
-	{
-		this.title=title;
-		this.details=details;
+
+	public AboutUsModel() {
+	}
+
+	public AboutUsModel(String title, String details) {
+		this.title = title;
+		this.details = details;
 	}
 
 }
--- src/main/java/com/model/spring/TeamModel.java
+++ src/main/java/com/model/spring/TeamModel.java
@@ -20,58 +20,92 @@ public class TeamModel {
 	private String details;
 	private String fb_id;
 	private String twiter_id;
+
+	private transient String fbURL;
+	private transient String twitterURL;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPost() {
 		return post;
 	}
+
 	public void setPost(String post) {
 		this.post = post;
 	}
+
 	public String getImage() {
 		return image;
 	}
+
 	public void setImage(String image) {
 		this.image = image;
 	}
+
 	public String getDetails() {
 		return details;
 	}
+
 	public void setDetails(String details) {
 		this.details = details;
 	}
+
 	public String getFb_id() {
 		return fb_id;
 	}
+
 	public void setFb_id(String fb_id) {
 		this.fb_id = fb_id;
 	}
+
 	public String getTwiter_id() {
 		return twiter_id;
 	}
+
 	public void setTwiter_id(String twiter_id) {
 		this.twiter_id = twiter_id;
 	}
-	
-	public TeamModel(){}
-	
+
+	public String getFbURL() {
+		return "https://facebook.com/" + fb_id;
+	}
+
+	public void setFbURL(String fbURL) {
+		this.fbURL = fbURL;
+	}
+
+	public String getTwitterURL() {
+		return "https://twitter.com/" + twiter_id;
+	}
+
+	public void setTwitterURL(String twitterURL) {
+		this.twitterURL = twitterURL;
+	}
+
+	public TeamModel() {
+	}
+
 	public TeamModel(String name, String post, String image, String details, String fb_id, String twiter_id) {
-		this.name=name;
-		this.post=post;
-		this.image=image;
-		this.details=details;
-		this.fb_id=fb_id;
-		this.twiter_id=twiter_id;
+		this.name = name;
+		this.post = post;
+		this.image = image;
+		this.details = details;
+		this.fb_id = fb_id;
+		this.twiter_id = twiter_id;
 	}
 
 }
